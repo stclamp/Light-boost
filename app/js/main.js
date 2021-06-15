8d58579ab7a566d5e65d0a979db9e26b30cd2ed1
Pavel <whatisclamp@gmail.com>
2021-06-15 15:06:51 +0300

    Slider and more

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
 $(function () {
+    $('.promo__slider').slick({
+        infinite: true,
+        autoplay: true,
+        fade: true,
+        prevArrow: `<button type="button" class="slick-prev"><img src='../images/slider-left.svg'></button>`,
+        nextArrow: `<button type="button" class="slick-next"><img src='../images/slider-right.svg'></button>`
+    });
+});
 
-})
+const tabBtns = document.querySelectorAll('[data-tabs-btn');
+const tabContent = document.querySelectorAll('[data-tabs-field]');
+
+for (const tab of tabBtns) {
+    tab.addEventListener('click', () => {
+        tabBtns.forEach(item => {
+            if (tab === item) {
+                item.classList.add('offers__btn--active');
+            } else {
+                item.classList.remove('offers__btn--active');
+            }
+        });
+    });
+    tabContent.forEach(item => {
+        if (item.dataset.tabsField === tab.dataset.tabsBtn) {
+            item.classList.remove('hidden');
+        } else {
+            item.classList.add('hidden');
+        }
+    });
+}
