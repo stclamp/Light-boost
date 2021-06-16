7c45846359956dbef6638f3fa1e30d168cb74ce6
Pavel <whatisclamp@gmail.com>
2021-06-16 20:45:55 +0300

    Add screens

--- app/js/main.js
+++ app/js/main.js
@@ -8,24 +8,89 @@ $(function () {
     });
 });
 
-const tabBtns = document.querySelectorAll('[data-tabs-btn');
-const tabContent = document.querySelectorAll('[data-tabs-field]');
+//TABS
 
-for (const tab of tabBtns) {
-    tab.addEventListener('click', () => {
-        tabBtns.forEach(item => {
-            if (tab === item) {
-                item.classList.add('offers__btn--active');
-            } else {
-                item.classList.remove('offers__btn--active');
-            }
-        });
+const tabBtn = document.querySelectorAll('.offers__btn');
+const tabsContent = document.querySelectorAll('.offers__cards')
+const tabBtns = document.querySelector('.offers__btns')
+
+function hideTabContent() {
+    tabsContent.forEach(item => {
+        item.classList.add('hidden');
+        item.classList.remove('show', 'fade');
     });
-    tabContent.forEach(item => {
-        if (item.dataset.tabsField === tab.dataset.tabsBtn) {
-            item.classList.remove('hidden');
-        } else {
-            item.classList.add('hidden');
-        }
+
+    tabBtn.forEach(item => {
+        item.classList.remove('offers__btn--active');
     });
 }
+
+function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hidden');
+    tabBtn[i].classList.add('offers__btn--active');
+}
+
+hideTabContent();
+showTabContent();
+
+tabBtns.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains('offers__btn')) {
+        tabBtn.forEach((item, i) => {
+            if (target == item) {
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    }
+});
+
+// TIMER 
+
+const deadline = '2021-06-30 00:00';
+
+function getTimeRemainig(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor((t / (1000 * 60 * 60 * 24)));
+    const hours = Math.floor((t / (1000 * 60 * 60) % 24));
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds,
+    };
+}
+
+
+
+function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+        const t = getTimeRemainig(endtime);
+
+        days.innerHTML = t.days;
+        hours.innerHTML = t.hours;
+        minutes.innerHTML = t.minutes;
+        seconds.innerHTML = t.seconds;
+
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+}
+
+setClock('.leveling__timer', deadline);
